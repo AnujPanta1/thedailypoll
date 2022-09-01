b58d86bc0d8ab4ed9d2c14bc2a2a73392161a189
anujpanta1 <anuj1panta@gmail.com>
2022-09-01 11:58:30 -0700

    quick

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import TopHeader from "./components/TopHeader";
 import MainSection from "./components/MainSection";
-import React, {useState } from "react";
+import React, { useState } from "react";
 import SubmitionWindow from "./components/SubmitionWindow";
 // eslint-disable-next-line
 import AboutSection from "./components/AboutSection";
 function App() {
+    const [mode, setMode] = useState("voting");
 
-  const [mode, setMode] = useState("voting");
+    const flipMode = () => {
+        if (mode === "voting") {
+            setMode("creating");
+        } else if (mode === "creating") {
+            setMode("voting");
+        } else {
+            setMode("voting");
+        }
+    };
 
-  const flipMode = () => {
-    if(mode ==="voting"){
-      setMode("creating");
-    }else if(mode ==="creating"){
-      setMode("voting");
-    }else{
-      setMode("voting");
-    }
-  }
-
-  return (
-    <div className="App">
-      <TopHeader flipMode={flipMode}></TopHeader>
-      <MainSection></MainSection>
-      {mode === "creating" ? <SubmitionWindow flipMode={flipMode}></SubmitionWindow> : <React.Fragment></React.Fragment>}
-    </div>
-  );
+    return (
+        <div className="App ap">
+            <TopHeader flipMode={flipMode}></TopHeader>
+            <MainSection></MainSection>
+            {mode === "creating" ? (
+                <SubmitionWindow flipMode={flipMode}></SubmitionWindow>
+            ) : (
+                <React.Fragment></React.Fragment>
+            )}
+        </div>
+    );
 }
 
 export default App;
